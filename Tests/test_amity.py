f9f96b931533fca07212b467e2a3cdda3853d8d6
Denis Gathondu <denis.gathondu@andela.com>
2017-03-01 13:46:04 +0300

    [Feature #1] Lay out the tests for functionality

--- Tests/test_amity.py
+++ Tests/test_amity.py
@@ -8,3 +8,13 @@ class TestAmity(unittest.TestCase):
     def setUp():
         a = Amity
 
+    # Test that a room is created
+    # Test that a person is added
+    # Test that a person is reallocated
+    # Test that people are loaded
+    # Test that allocations are printed
+    # Test that unallocations are printed
+    # Test that print room prints all the people in the room
+    # Test that state is saved in the database
+    # Test that state is loaded
+
