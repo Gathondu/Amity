dc198b01ae17c85246983d6166cb4ca2cb749fa3
Denis Gathondu <denis.gathondu@andela.com>
2017-03-04 11:20:17 +0300

    [Feature #2] Refactored room class file

--- model/room.py
+++ model/room.py
@@ -4,54 +4,67 @@
 
 class Room:
 
-    name = "Room Name"
-    room_type = "Room Type"
-    _MAX_SPACE = 0
-    _occupied_spaces = 0
-    _occupants = set()
+    def __init__(self, name='Room Name', room_type='Room Type', max_space=0):
+        self._MAX_SPACE = max_space
+        self._occupants = []
+        self.name = name
+        self.room_type = room_type
 
     def __str__(self):
         return '{} {} has {} occupant[s].'.format(self.room_type, self.name,
-                                                  self.occupied_spaces())
+                                                  self.occupied_spaces)
+
+    @property
+    def max_space(self):
+        return self._MAX_SPACE
+
+    @property
+    def occupants(self):
+        if self._occupants == []:
+            return '{} is empty.'.format(self.name)
+        return self._occupants
+
+    @property
+    def occupied_spaces(self):
+        return len(self._occupants)
+
+    @max_space.setter
+    def _max_space(self, space):
+        if not isinstance(space, int):
+            return '{} is not an integer!!!'
+        self._MAX_SPACE = space
+        return "maximum number of people allowed to be assigned to {} has"
+        " been updated to {}.".format(self.name, space)
+
+    @property
+    def availability(self):
+        return self.max_space - self.occupied_spaces
 
     def add_person(self, person_name):
-        if self.check_availability():
+        if self.availability:
             if person_name in self._occupants:
                 return '{} already assigned space in {}'.format(person_name,
                                                                 self.name)
-            self._occupied_spaces += 1
-            self._occupants.add(person_name)
+            self._occupants.append(person_name)
             return '{} assigned space in {}'.format(person_name, self.name)
-
-    def check_availability(self):
-        return self._MAX_SPACE - self.occupied_spaces()
-
-    def occupants(self):
-        return self._occupants
-
-    def occupied_spaces(self):
-        return self._occupied_spaces
 
 
 class Office(Room):
 
     def __init__(self, name):
+        self._MAX_SPACE = 0
         self.name = name
         self.room_type = "office"
-        self._set_max_space()
-        self._occupants = set()
-
-    def _set_max_space(self):
-        self._MAX_SPACE = 6
+        self._max_space = 6
+        self._occupants = []
 
 
 class LivingSpace(Room):
 
     def __init__(self, name):
+        self._MAX_SPACE = 0
         self.name = name
         self.room_type = "living space"
-        self._set_max_space()
-        self._occupants = set()
-
-    def _set_max_space(self):
-        self._MAX_SPACE = 4
+        self._max_space = 4
+        self._occupied_spaces = 0
+        self._occupants = []
