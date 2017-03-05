7d382814026fd3332c394c63f88384043d97be91
Denis Gathondu <denis.gathondu@andela.com>
2017-03-05 22:47:17 +0300

    [Feature #141062289] Add tests for person and functionality for add person method

--- model/amity.py
+++ model/amity.py
@@ -1,29 +1,87 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
+
+
 from model.person import Fellow, Staff
 from model.room import Office, LivingSpace
 
 
 class Amity:
-    offices = []
-    living_spaces = []
-    rooms = [["name", "type", "allocated_spaces"]]
-    rooms_allocations = {
-        "offices": {
-            "name": "",
-            "allocated_spaces": 0
-        },
-        "living_spaces": {
-            "name": "",
-            "allocated_spaces": 0
-        }
-    }
-    staff = []
-    fellows = []
+    offices = {}
+    living_spaces = {}
+    rooms_allocations = {}
+    staff = {}
+    fellow = {}
+    true = ('y', 'yes')
 
-    def add_person(first_name, last_name, position, living_space=None):
+    def __init__(self):
+        self.offices = {}
+        self.living_spaces = {}
+        self.rooms_allocations = {}
+        self.staff = {}
+        self.fellow = {}
+
+    def add_person(self, *args):
         """This function employs  a new new person as  a staff or fellow."""
-        pass
+        # import pdb; pdb.set_trace()
+        if len(args) < 1:
+            raise ValueError('please specify name type and' +
+                             ' if person wants living space in case of fellows'
+                             )
+        for person in args:
+            details = person.lower().split()
+            if len(details) > 4:
+                raise ValueError('give just two names, type of person and' +
+                                 ' y if its a fellow who wants accomodation')
+            elif len(details) < 2:
+                raise ValueError('give at least a name and the type of person')
+
+            name = [name for name in details if name not in
+                    ('staff', 'fellow', 'y', 'yes')]
+            if ''.join(name).isalpha():
+                name = ' '.join(name)
+            else:
+                raise ValueError('name must be alphabetic chars.' +
+                                 ' {} is incorrect'.format(name))
+            person_type = [typ for typ in details if typ in
+                           ('staff', 'fellow')]
+            wants_livingspace = [space for space in details if space
+                                 in ('y', 'yes')]
+            if len(wants_livingspace) == 1:
+                wants_livingspace = ''.join(wants_livingspace)
+            elif len(wants_livingspace) > 1:
+                raise ValueError(
+                    'only specify once if fellow wants living space'
+                    )
+            else:
+                wants_livingspace = ''
+
+            if len(person_type) > 1:
+                raise ValueError('person type specified twice')
+            elif len(person_type) < 1:
+                raise ValueError('person type not specified.' +
+                                 ' Should be staff or fellow')
+            else:
+                person_type = ''.join(person_type)
+
+            if person_type == 'staff' and wants_livingspace in self.true:
+                raise ValueError('staff cannot be assigned LivingSpace')
+            if person_type == 'staff':
+                if name in self.staff.keys():
+                    raise ValueError('staff {} already exsists.'.format(name))
+                else:
+                    self.staff[name] = Staff(name)
+                    return 'staff {} added'.format(name)
+            if person_type == 'fellow':
+                if name in self.fellow.keys():
+                    raise ValueError('fellow {} already exsists.'.format(name))
+                else:
+                    if wants_livingspace in self.true:
+                        fellow = Fellow(name, True)
+                    else:
+                        fellow = Fellow(name)
+                    self.fellow[name] = fellow
+                    return 'fellow {} added'.format(name)
 
     def allocate_space(name, position, livaing_space):
         """This function allocates space to new employees."""
@@ -33,7 +91,7 @@ class Amity:
         """This function checks for the availability of rooms in amity"""
         pass
 
-    def create_room(type, room_name):
+    def create_room(*args):
         """This function creates a new room in amity"""
         pass
 
--- tests/test_amity.py
+++ tests/test_amity.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import unittest
+from unittest import TestCase, skip
 from model.amity import Amity
 
 # edge cases
@@ -10,69 +10,114 @@ from model.amity import Amity
 # input vs output don't complicate
 
 
-class TestAmity(unittest.TestCase):
+class TestAmity(TestCase):
 
     def setUp(self):
-        # offices: valhalla, hogwarts, occulus, krypton
-        # livingspaces outside, dojo
-        a = Amity()
-        self.reallocate_person = {  # change variable name
-            "1": {
-                "name": "denis gathondu",
-                "type": "staff",
-                "room": "valhalla"
-                },
-            "2": {
-                "name": "sylvia sly",
-                "type": "fellow",
-                "livingspace": True,
-                "room": {
-                    "office": "valhalla",
-                    "livingspace": "outside"
-                    }
-                },
-            "3": {
-                "name": "shem shem",
-                "type": "fellow",
-                "livingspace": False,
-                "room": "valhalla"
-                }
-        }
+        self.a = Amity()
+        self.occulus = self.a.create_room('office occulus')
+        self.dojo = self.a.create_room('livingspace dojo')
+        self.staff = self.a.add_person('denis gathondu staff')
+        self.fellow = self.a.add_person('denis gathondu fellow')
+        self.fellow_with_living_space = self.a.add_person(
+            'shem mwangi fellow y'
+            )
 
     def test_room_is_created(self):
-        room = Amity.create_room("office", "occulus")
-        self.assertEqual("office occulus created", room)
+        self.assertEqual("office occulus created", self.occulus)
+        self.assertEqual("livingspace dojo created", self.dojo)
 
-    def test_staff_is_added(self):
-        person = Amity.add_person("DeNis", "Gathondu", "staff")
-        self.assertEqual("staff denis gathondu created", person)
+    def test_room_is_created_correctly(self):
+        from model.room import Office, LivingSpace
+        self.assertIsInstance(self.occulus, Office)
+        self.assertIsInstance(self.dojo, LivingSpace)
+
+    def test_multiple_rooms_creation(self):
+        multi_rooms = self.a.create_room(
+            'office valhalla', 'livingspace outside', 'office krypton',
+            'livingspace flats'
+            )
+        self.assertEqual(
+            'offices valhalla, krypton and livingspaces outside, ' +
+            'flats created',
+            multi_rooms
+            )
+
+    def test_empty_room_name_raises_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.a.create_room()
+
+    def test_room_name_can_only_be_a_string(self):
+        with self.assertRaises(ValueError):
+            self.a.create_room(4)
+            self.a.create_room('4.7')
+
+    def test_room_name_cannot_be_duplicated(self):
+        with self.assertRaises(ValueError):
+            self.a.create_room('office occulus')
+            self.a.create_room('livingspace occulus')
+            self.a.create_room('livingspace dojo')
+            self.a.create_room('office dojo')
+
+    def test_room_type_must_be_specified_upon_creation(self):
+        with self.assertRaises(ValueError):
+            self.a.create_room('krypton')
+
+    def test_room_type_and_name_must_be_space_separated(self):
+        with self.assertRaises(ValueError):
+            self.a.create_room('officekrypton')
+
+    def test_person_is_added(self):
+        self.assertEqual("staff denis gathondu added", self.staff)
+        self.assertEqual("fellow denis gathondu added", self.fellow)
+        self.assertEqual(
+            "fellow shem mwangi added", self.fellow_with_living_space
+            )
+
+    def test_person_is_added_correctly(self):
+        from model.person import Fellow, Staff
+        self.assertIsInstance(self.a.fellow['denis gathondu'], Fellow)
+        self.assertIsInstance(self.a.staff['denis gathondu'], Staff)
 
     def test_staff_cannot_request_livingspace(self):
         with self.assertRaises(ValueError):
-            Amity.add_person("DeNis", "Gathondu", "staff", "yes")
-
-    def test_fellow_is_added(self):
-        person = Amity.add_person("DeNis", "Gathondu", "fellow")
-        self.assertEqual(
-            "fellow denis gathondu created and doesn't require a living space",
-            person
-            )
+            self.a.add_person('dan otieno staff y')
 
     def test_fellow_requires_livingspace_is_added_correctly(self):
-        person = Amity.add_person("DeNis", "Gathondu", "fellow", "yes")
-        self.assertEqual(
-            "fellow denis gathondu created and requires a living space",
-            person
-            )
+        fellow = [
+            fellow for fellow in self.a.fellow.values()
+            if fellow.wants_living_space is True
+            ]
+        self.assertTrue(fellow[0].wants_living_space)
+
+    def test_fellow_not_requiring_livingspace_is_added_correctly(self):
+        fellow = [
+            fellow for fellow in self.a.fellow.values()
+            if fellow.wants_living_space is False
+            ]
+        self.assertFalse(fellow[0].wants_living_space)
+
+    def test_person_name_is_required(self):
+        with self.assertRaises(ValueError):
+            self.a.add_person()
 
     def test_person_type_is_required(self):
-        with self.assertRaises(TypeError):
-            Amity.add_person("denis", "gathondu")
+        with self.assertRaises(ValueError):
+            self.a.add_person('dan otieno')
 
-    @unittest.skip("WIP")
+    def test_person_type_cannot_be_specified_twice(self):
+        with self.assertRaises(ValueError):
+            self.a.add_person('dan otieno y y')
+
+    def test_person_name_and_type_should_be_space_separated(self):
+        with self.assertRaises(ValueError):
+            self.a.add_person('danotienostaff')
+
     def test_person_should_not_be_duplicated(self):
-        pass
+        with self.assertRaises(ValueError):
+            self.a.add_person('denis gathondu staff')
+            self.a.add_person('shem mwangi fellow')
 
+    @skip('WIP')
     def test_that_fellow_with_livingspace_is_reallocated_livingspace(self):
         reallocate = Amity.reallocate_person(self.reallocate_person["2"])
         self.assertEqual(
@@ -84,6 +129,7 @@ class TestAmity(unittest.TestCase):
         #     reallocate[1]
         #     )
 
+    @skip('WIP')
     def test_that_fellow_with_livingspace_is_reallocated_office(self):
         reallocate = Amity.reallocate_person(self.reallocate_person["2"])
         self.assertEqual(
@@ -95,6 +141,7 @@ class TestAmity(unittest.TestCase):
         #     reallocate[0]
         #     )
 
+    @skip('WIP')
     def test_that_fellow_without_livingspace_is_reallocated_office(self):
         reallocate = Amity.reallocate_person(self.reallocate_person["3"])
         self.assertEqual(
@@ -102,6 +149,7 @@ class TestAmity(unittest.TestCase):
             reallocate
             )
 
+    @skip('WIP')
     def test_that_staff_is_reallocated_office(self):
         reallocate = Amity.reallocate_person(self.reallocate_person["1"])
         self.assertEqual(
@@ -109,32 +157,35 @@ class TestAmity(unittest.TestCase):
             reallocate
             )
 
-    @unittest.skip("WIP")
+    @skip("WIP")
     def test_people_are_loaded(self):
         pass
 
+    @skip('WIP')
     def test_allocations_are_printed(self):
         allocations = Amity.print_allocations()
         self.assertIsNotNone(allocations)
 
-    @unittest.skip("WIP")
+    @skip("WIP")
     def test_unallocations_are_printed(self):
         unallocations = Amity.print_unallocated()
         self.assertIsNotNone(unallocations)
 
-    @unittest.skip("WIP")
+    @skip("WIP")
     def test_print_room_prints_all_people_in_room(self):
         room = Amity.create_room("office", "krypton")
         # room.occupied_spaces = 2
         people = Amity.print_room(room.name)
         self.assertEqual(2, len(people))
 
-    @unittest.skip("WIP")
+    @skip("WIP")
     def test_state_is_saved_in_database(self):
         pass
 
+    @skip('WIP')
     def test_state_is_loaded(self):
         pass
 
+    @skip('WIP')
     def test_check_room_availability(self):
         pass
